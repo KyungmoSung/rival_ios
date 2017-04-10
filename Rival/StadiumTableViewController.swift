349a7aee5c1617884071168e076de3b31c377c93
Sung Kyungmo <skm280@naver.com>
2017-04-10 14:15:22 +0900

    Show activityIndicator while get database Send indexpath in the tag of the button

--- Rival/StadiumTableViewController.swift
+++ Rival/StadiumTableViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class StadiumTableViewController: UITableViewController {
     let com = Communication()
+    let indicator = ActivityIndicator()
+    var activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        activityIndicator = indicator.setActivityIndicator(view: self.view)
+        activityIndicator.startAnimating()
+        activityIndicator.backgroundColor = UIColor.white
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -25,6 +31,7 @@ class StadiumTableViewController: UITableViewController {
     
     // MARK: - Reload Data
     func reloadTableStadium(){
+        activityIndicator.stopAnimating()
         self.tableView.reloadData()
     }
     
@@ -45,70 +52,30 @@ class StadiumTableViewController: UITableViewController {
         return Communication.stadium.count
     }
     
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "stadiumCell", for: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stadiumCell", for: indexPath)  as! StadiumTableViewCell
         
         let stadium = Communication.stadium[indexPath.row]
         
-        cell.textLabel?.text=stadium.stadium_name
-        cell.detailTextLabel?.text=stadium.road_address
-     
-     return cell
-     }
+        cell.label_name.text=stadium.stadium_name
+        cell.label_location.text=stadium.road_address
+        cell.button_detail.tag = indexPath.row
+        return cell
+    }
+    
+    @IBAction func tabButtonDetail(_ sender: UIButton) {
+        let buttonRow = sender.tag
+        StadiumDetailViewController.stadium = Communication.stadium[buttonRow]
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        let indexPath = tableView.indexPathForSelectedRow
-        
-        let stadium = Communication.stadium[(indexPath?.row)!]
-        
-        let detailViewController = segue.destination as! StadiumDetailViewController
-        detailViewController.stadium = stadium
-        
+        if( segue.identifier == "select"){
+            var indexPath = self.tableView.indexPathForSelectedRow
+            let stadium = Communication.stadium[(indexPath?.row)!]
+            let detailViewController = segue.destination as! AddMatchingViewController
+            detailViewController.selectedStadium = stadium.stadium_name
+        }
     }
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
